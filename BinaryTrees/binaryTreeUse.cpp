8d80860112ad1c1aa24c1181aaac38b09cbcfa85
Namanpreet Kaur <69746852+naman-gurnaaz@users.noreply.github.com>
2022-10-08 15:04:28 +0530

    Bianry Trees

--- BinaryTrees/binaryTreeUse.cpp
+++ BinaryTrees/binaryTreeUse.cpp
@@ -98,30 +98,129 @@ void printLevelWise(BinaryTreeNode<int> *root){
     }
 }
 
-
-
-int travelTime(int a[], int size){
-    int count = 0;
-    int home  = a[0];
-    int dest = a[size-1];
-    for(int i = 0; i<size; i++){
-        if(a[i]<=home){
-            home = a[i];
-            count++;
-        }
+int countNodes(BinaryTreeNode<int> *root){
+    if(root == nullptr){
+        return 0;
     }
-    return count;
+    return 1+countNodes(root->left) + countNodes(root->right);
 }
 
-int main(){
-    //BinaryTreeNode<int> *root = takeInputLevelWise();
-    //printLevelWise(root);
 
-    int n;
-    cin >> n;
-    int arr[n];
-    for(int i =0; i<n; i++){
-        cin >> arr[i];
+void inOrder(BinaryTreeNode<int> *root){
+    if(root == nullptr){
+        return;
     }
-    cout << travelTime(arr, n)<< endl;
+    inOrder(root->left);
+    cout << root->data << " ";
+    inOrder(root->right);
+}
+
+void preOrder(BinaryTreeNode<int> *root){
+    if(root == nullptr){
+        return;
+    }
+    cout << root ->data << " ";
+    preOrder(root->left);
+    preOrder(root->right);
+}
+void postOrder(BinaryTreeNode<int> *root){
+    if(root == nullptr){
+        return;
+    }
+    postOrder(root->left);
+    postOrder(root->right);
+    cout << root->data << " ";
+}
+
+
+BinaryTreeNode<int> * buildTreeHelper(int* in, int* pre, int inS, int inE, int preS, int preE){
+    if(inS > inE){
+        return nullptr;
+    }
+    int rootData = pre[preS];
+
+    int rootIndex = -1;
+    for(int i = inS; i<=inE; i++){
+        if(in[i] == rootData){
+            rootIndex = i;
+            break;
+        }
+    }
+
+    //left sub-tree
+    int lInS = inS;
+    int lInE = rootIndex-1;
+    int lPreS = preS + 1;
+    int lPreE = lInE - lInS + lPreS;
+
+    //right sub tree
+    int rPreS = lPreE+1;
+    int rPreE = preE;
+    int rInS = rootIndex + 1;
+    int rInE = inE;
+
+    BinaryTreeNode<int> *root = new BinaryTreeNode<int>(rootData);
+    root->left = buildTreeHelper(in, pre, lInS, lInE, lPreS, lPreE);
+    root->right = buildTreeHelper(in, pre, rInS, rInE, rPreS, rPreE);
+
+    return root;
+}
+
+BinaryTreeNode<int> * buildTree(int* in, int* pre, int size){
+    return buildTreeHelper(in, pre, 0, size-1, 0, size-1);
+}
+
+int height(BinaryTreeNode<int> *root){
+    if(root == nullptr){
+        return 0;
+    }
+
+    return 1+ max(height(root->left), height(root->right));
+}
+
+int diameter(BinaryTreeNode<int> *root){
+    if(root == nullptr){
+        return 0;
+    }
+
+    int op1 = height(root->left) + height(root->right);
+    int op2 = diameter(root->left);
+    int op3 = diameter(root->right);
+
+    return max(op1, max(op2, op3));
+}
+
+pair<int, int> heightDiameter(BinaryTreeNode <int> *root)
+{
+    if(root == nullptr){
+        pair<int, int> p;
+        p.first = 0;
+        p.second = 0;
+        return p;
+    }
+
+    pair<int, int> leftans = heightDiameter(root->left);
+    pair<int, int> rightans = heightDiameter(root->right);
+    int ld = leftans.second;
+    int rd = rightans.second;
+    int lh = leftans.first;
+    int rh = rightans.first;
+
+    int height = 1 + max(lh, rh); 
+    int diameter = max(lh+rh, max(ld,rd));
+
+    pair<int, int> ans;
+    ans.first = height;
+    ans.second = diameter;
+
+    return ans;
+}
+
+
+// 1 2 3 4 5 6 7 -1 -1 -1 -1 8 9 -1 -1 -1 -1 -1 -1 
+int main(){
+    BinaryTreeNode<int> *root= takeInputLevelWise();
+    printBinaryTree(root);
+    pair<int, int> n = heightDiameter(root);
+    cout << n.first <<"   " << n.second << endl;
 }
